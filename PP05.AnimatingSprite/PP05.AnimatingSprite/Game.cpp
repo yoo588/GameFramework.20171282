ca6fa8242776bb6a6f29b97ab7ae8d88489fb08c
학과 게임 <hoseogame@hoseogame.onmicrosoft.com>
2018-09-21 10:57:52 +0900

    Hellow world

---
+++ PP05.AnimatingSprite/PP05.AnimatingSprite/Game.cpp
@@ -0,0 +1,79 @@
+#include "Game.h"
+#include "SDL.h"
+#include <iostream>
+#include <windows.h>
+
+bool Game::init(const char* title, int xpos, int ypos,
+	int width, int height, bool fullscreen)
+{
+
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+	{
+		m_pWindow = SDL_CreateWindow(title, xpos, ypos,
+			width, height, fullscreen);
+
+		if (m_pWindow != 0)
+		{
+			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
+		}
+		m_bRunning = true;
+
+		
+	}
+	else {
+		return false;
+	}
+
+	SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/animate.bmp");
+	m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer,
+		pTempSurface);
+
+	SDL_FreeSurface(pTempSurface);
+
+	m_sourceRectangle.w = 128;
+	m_sourceRectangle.h = 82;
+
+	m_destinationRectangle.x = m_sourceRectangle.x = 0;
+	m_destinationRectangle.y = m_sourceRectangle.y = 0;
+	m_destinationRectangle.w = m_sourceRectangle.w;
+	m_destinationRectangle.h = m_sourceRectangle.h;
+
+}
+
+void Game::render()
+{
+	SDL_RenderClear(m_pRenderer);
+	SDL_RenderCopy(m_pRenderer, m_pTexture,
+		&m_sourceRectangle, &m_destinationRectangle);
+	SDL_RenderPresent(m_pRenderer);
+}
+
+void Game::clean()
+{
+	std::cout << "cleaning game\n";
+	SDL_DestroyWindow(m_pWindow);
+	SDL_DestroyRenderer(m_pRenderer);
+	SDL_Quit();
+}
+
+void Game::update()
+{
+	m_sourceRectangle.x = 128 * int(((SDL_GetTicks() / 100) % 6));
+}
+
+void Game::handleEvents()
+{
+	SDL_Event event;
+	if (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			m_bRunning = false;
+			break;
+		default:
+			break;
+		}
+	}
+}
